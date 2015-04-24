53772b49906d8b4132528b53485374148c791229
Markus Bausdorf <m.bausdorf@gmail.com>
2015-04-24 15:54:26 +0200

    Uebung 1 ist fertig.

--- src/de/htw/ds/sync/FileCopyMultiThreaded.java
+++ src/de/htw/ds/sync/FileCopyMultiThreaded.java
@@ -8,66 +8,77 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-
 /**
- * Demonstrates copying a file using a single thread. Note that this class is declared final because
- * it provides an application entry point, and therefore not supposed to be extended.
+ * Demonstrates copying a file using a single thread. Note that this class is
+ * declared final because it provides an application entry point, and therefore
+ * not supposed to be extended.
  */
 @TypeMetadata(copyright = "2008-2015 Sascha Baumeister, all rights reserved", version = "0.3.0", authors = "Sascha Baumeister")
 public final class FileCopyMultiThreaded {
 
 	/**
-	 * Copies a file. The first argument is expected to be a qualified source file name, the second
-	 * a qualified target file name.
-	 * @param args the VM arguments
-	 * @throws IOException if there's an I/O related problem
+	 * Copies a file. The first argument is expected to be a qualified source
+	 * file name, the second a qualified target file name.
+	 * 
+	 * @param args
+	 *            the VM arguments
+	 * @throws IOException
+	 *             if there's an I/O related problem
 	 */
-	static public void main (final String[] args) throws IOException {
+	static public void main(final String[] args) throws IOException {
 		final Path sourcePath = Paths.get(args[0]);
-		if (!Files.isReadable(sourcePath)) throw new IllegalArgumentException(sourcePath.toString());
+		if (!Files.isReadable(sourcePath))
+			throw new IllegalArgumentException(sourcePath.toString());
 
 		final Path sinkPath = Paths.get(args[1]);
-		if (sinkPath.getParent() != null && !Files.isDirectory(sinkPath.getParent())) throw new IllegalArgumentException(sinkPath.toString());
+		if (sinkPath.getParent() != null && !Files.isDirectory(sinkPath.getParent()))
+			throw new IllegalArgumentException(sinkPath.toString());
 
-		// Files.copy(sourcePath, sinkPath, StandardCopyOption.REPLACE_EXISTING);
-		
-		try(PipedInputStream pInputStream = new PipedInputStream(4*1024))
-		{
-			try(PipedOutputStream pOutputStream = new PipedOutputStream(pInputStream))
-			{
-				Thread firstThread = new Thread(new Transporter(Files.newInputStream(sourcePath), pOutputStream));
-				Thread secondThread = new Thread(new Transporter(pInputStream, Files.newOutputStream(sinkPath)));
-				
-				firstThread.start();
-				secondThread.start();
-			}
+		// Files.copy(sourcePath, sinkPath,
+		// StandardCopyOption.REPLACE_EXISTING);
+
+		PipedInputStream pInputStream = new PipedInputStream(4 * 1024);
+		PipedOutputStream pOutputStream = new PipedOutputStream(pInputStream);
+
+		Thread firstThread = new Thread(new Transporter(Files.newInputStream(sourcePath), pOutputStream));
+		Thread secondThread = new Thread(new Transporter(pInputStream, Files.newOutputStream(sinkPath)));
+
+		firstThread.start();
+		secondThread.start();
+
+		try {
+			System.out.println("Waiting for threads to finish.");
+			firstThread.join();
+			secondThread.join();
+		} catch (InterruptedException e) {
+			System.out.println("Main thread Interrupted");
 		}
-		
+
+		pInputStream.close();
 		System.out.println("done.");
+
 	}
 
-	private static class Transporter implements Runnable{
+	private static class Transporter implements Runnable {
 		InputStream inputStream;
 		OutputStream outputStream;
 
-		public Transporter(InputStream is, OutputStream os)
-		{
+		public Transporter(InputStream is, OutputStream os) {
 			this.inputStream = is;
 			this.outputStream = os;
 		}
 
-		public void run()
-		{
-			try{
+		public void run() {
+			try {
 				final byte[] buffer = new byte[0x10000];
-				for (int bytesRead = this.inputStream.read(buffer); bytesRead != -1; bytesRead = this.inputStream.read(buffer)) {
+				for (int bytesRead = this.inputStream.read(buffer); bytesRead != -1; bytesRead = this.inputStream
+						.read(buffer)) {
 					this.outputStream.write(buffer, 0, bytesRead);
 				}
-			} catch (IOException e)
-			{
-				//bad luck
+			} catch (IOException e) {
+				// bad luck
 			}
-			
+
 		}
 	}
 }
