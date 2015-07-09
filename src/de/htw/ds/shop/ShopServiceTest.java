957a2ef40fa42c473ce575827a840c365fbd5b25
Stefan Kaufhold <cere@fastmail.fm>
2015-07-09 20:41:34 +0200

    mehr tests

--- src/de/htw/ds/shop/ShopServiceTest.java
+++ src/de/htw/ds/shop/ShopServiceTest.java
@@ -1,10 +1,14 @@
 package de.htw.ds.shop;
 
 import de.sb.java.xml.Namespaces;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 import javax.xml.ws.Service;
@@ -34,28 +38,28 @@ public class ShopServiceTest  {
             serviceProxy = proxyFactory.getPort(ShopService.class);
         }
         catch (MalformedURLException e) {
-            throw new AssertionError("Server-URL ungültig");
+            throw new AssertionError("Server-URL ungï¿½ltig");
         }
     }
 
     @Before
     public void preTest() {
-        try {
+        /*try {
             sp = server.getJdbcConnector().getConnection().setSavepoint("UnitTest");
         }
         catch(SQLException e) {
             throw new AssertionError("Konnte keinen Savepoint erstellen");
-        }
+        }*/
     }
 
     @After
     public void postTest() {
-        try {
+        /*try {
             server.getJdbcConnector().getConnection().rollback(sp);
         }
         catch(SQLException e) {
             throw new AssertionError("Konnte den Zustand vor dem Test nicht wiederherstellen");
-        }
+        }*/
     }
 
     @Test
@@ -65,8 +69,37 @@ public class ShopServiceTest  {
     }
 
     @Test
-    public void testQueryarticles() {
+    public void testQueryArticles() {
         final Collection<Article> articles = serviceProxy.queryArticles();
         assertEquals("anzahl artikel", 3, articles.size());
+    }
+
+    @Test
+    public void testRegisterCustomer() {
+        final Customer testCustomer = new Customer();
+        testCustomer.setAlias("test");
+        testCustomer.setEmail("example@example.org");
+        testCustomer.setCity("1");
+        testCustomer.setGivenName("2");
+        testCustomer.setFamilyName("3");
+        testCustomer.setPhone("4");
+        testCustomer.setPostcode("5");
+        testCustomer.setStreet("6");
+        final long customerNum = serviceProxy.registerCustomer(testCustomer, "test");
+        assertThat(customerNum, not(0));
+
+        Customer foundCustomer = serviceProxy.queryCustomer("test", "test");
+
+        assertThat(foundCustomer.getIdentity(), is(customerNum));
+        assertThat(foundCustomer.getAlias(), is(testCustomer.getAlias()));
+    }
+
+    @Test
+    public void testQueryCustomer() {
+        Customer sascha = serviceProxy.queryCustomer("sascha", "sascha");
+
+        assertThat(sascha.getAlias(), is("sascha"));
+        assertThat(sascha.getFamilyName(), is("Baumeister"));
+        assertThat(sascha.getPostcode(), is("10999"));
     }
 }
